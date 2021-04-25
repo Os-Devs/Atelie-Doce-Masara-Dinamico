14f4243e0705cd5f26ab69157d80865bb612c1b5
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2021-04-25 20:14:29 -0300

    OffCanvas Tunado

--- js/main.js
+++ js/main.js
@@ -9,9 +9,20 @@ const CatalogoMain = () => {
   for (let i = 0; i < dadosMain.length; i++) {
     const show = `
     <div class="bolo${dadosMain[i].id}">
-      <a href="${dadosMain[i].link}"><img src="${dadosMain[i].image}" alt="${dadosMain[i].title}" /></a>
+      <img src="${dadosMain[i].image}" alt="${dadosMain[i].title}" type="button" data-bs-toggle="offcanvas" 
+      data-bs-target="#offcanvasBottom${dadosMain[i].id}" aria-controls="offcanvasBottom"/></a>
       <h3>${dadosMain[i].title}</h3>
       <p>${dadosMain[i].description}</p>
+    </div>
+    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasBottom${dadosMain[i].id}" aria-labelledby="offcanvasBottomLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasBottomLabel">${dadosMain[i].title}</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body small">
+        <img src="${dadosMain[i].image1}" alt="${dadosMain[i].title}">
+        <img src="${dadosMain[i].image2}" alt="${dadosMain[i].title}">
+      </div>
     </div>`
 
     catalogoVazio.insertAdjacentHTML('beforeend', show)
