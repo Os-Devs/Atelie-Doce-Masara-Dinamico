427ee2b718df4f8d17f0a5f07713565490c81eb2
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2021-04-25 18:42:50 -0300

    Catalogo dinâmico

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-import datasetMain from '../js/model/datasetMain.js'
-import datasetOthers from '../js/model/datasetOthers.js'
+import datasetMain from "./model/dadosImgMain.js";
+import datasetOthers from "./model/dadosCatalogos.js";
+
+const dadosMain = datasetMain;
+const dadosOthers = datasetOthers;
+const catalogoVazio = document.querySelector(".catalogo")
+
+const CatalogoMain = () => {
+  for (let i = 0; i < dadosMain.length; i++) {
+    const show = `
+    <div class="bolo${dadosMain[i].id}">
+      <a href="${dadosMain[i].link}"><img src="${dadosMain[i].image}" alt="${dadosMain[i].title}" /></a>
+      <h3>${dadosMain[i].title}</h3>
+      <p>${dadosMain[i].description}</p>
+    </div>`
+
+    catalogoVazio.insertAdjacentHTML('beforeend', show)
+  };
+};
+
+CatalogoMain()
